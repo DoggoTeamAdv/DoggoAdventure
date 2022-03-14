a96cffc17d8086f5c2e4bd20a32c6fd8836beb57
matsaki95 <matsaki95@tutanota.com>
2022-03-14 14:51:45 +0200

    Miso ton kosmo

--- src/main/java/com/github/DoggoTeamAdv/App.java
+++ src/main/java/com/github/DoggoTeamAdv/App.java
@@ -5,6 +5,6 @@ package com.github.DoggoTeamAdv;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("FU World!");
     }
 }
