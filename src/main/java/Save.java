712c701e117199882ef22859298b8058c8ff499f
CaptainEliasGR <esahlos12@gmail.com>
2022-04-26 16:05:54 +0300

    SaveLoad Test 26/4

---
+++ src/main/java/Save.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Save {
+    public static final Scanner scanner = new Scanner(System.in);
+}
